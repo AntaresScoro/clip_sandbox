65299109c7fa49f7cf16aebc5ff26030c2c7ce2e
AntaresScoro <antares.scoro@gmail.com>
2025-11-27 20:23:31 +0100

    Implement ClipsService with `findAll` method and integrate it into ClipsController

--- src/clips/clips.controller.ts
+++ src/clips/clips.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ClipsService } from './clips.service';
 
 @Controller('clips')
-export class ClipsController {}
+export class ClipsController {
+  constructor(private readonly clipsService: ClipsService) {}
+  @Get()
+  getClips():  string[] {
+    return this.clipsService.findAll();
+  }
+}
--- src/clips/clips.service.ts
+++ src/clips/clips.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ClipsService {}
+export class ClipsService {
+  findAll(): string[] {
+    return [
+      'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    ];
+  }
+}
