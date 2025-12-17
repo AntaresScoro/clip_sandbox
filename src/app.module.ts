54108cc4d30d09d1aef9d056e22e2b3dfe96eee1
AntaresScoro <antares.scoro@gmail.com>
2025-12-17 15:55:00 +0100

    Add `AuthModule` with controller, service, and DTO boilerplate, and integrate into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ClipsModule } from './clips/clips.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { UsersModule } from './users/users.module';
     ),
     ConfigModule.forRoot(),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
