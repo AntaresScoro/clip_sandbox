d46d1ce43a6e8e3ff8ff19851933ecf3a1875fc9
AntaresScoro <antares.scoro@gmail.com>
2026-01-09 01:33:25 +0100

    Refactor database configuration to use `MONGO_URI` from `.env`, enhance tests with `beforeAll` and `afterAll` lifecycle hooks, and update `test:e2e` script to support environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClipsModule } from './clips/clips.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ClipsModule,
-    MongooseModule.forRoot(
-      'mongodb://root:example@localhost:27018/clip_sandbox?authSource=admin',
-    ),
     ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ClipsModule,
     UsersModule,
     AuthModule,
   ],
--- src/clips/clips.service.spec.ts
+++ src/clips/clips.service.spec.ts
@@ -1,18 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClipsService } from './clips.service';
+import { Clip } from './schema/clip.schema';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('ClipsService', () => {
   let service: ClipsService;
-
+  const clipModelMock = {};
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ClipsService],
+      providers: [
+        ClipsService,
+        { provide: getModelToken(Clip.name), useValue: clipModelMock },
+      ],
     }).compile();
 
     service = module.get<ClipsService>(ClipsService);
   });
 
-  it('should be defined', () => {
+  test('should be defined', () => {
     expect(service).toBeDefined();
   });
 });
